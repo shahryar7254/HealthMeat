6fd91d74e424624c4f51255ae248298b18dcdf71
Shahryar-elityx <s.shabeer@elityx.com>
2026-09-19 11:23:27 +0500

    Update main entry point to LoginScreen in HealthMate app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
 import 'theme/app_theme.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class HealthMateApp extends StatelessWidget {
       title: 'HealthMate AI',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
